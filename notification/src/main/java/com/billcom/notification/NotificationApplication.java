d09b159dcaf8537466cdfc1e0eab27291b7537e9
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-06-06 18:04:37 +0100

    adding rabbit mq

--- notification/src/main/java/com/billcom/notification/NotificationApplication.java
+++ notification/src/main/java/com/billcom/notification/NotificationApplication.java
@@ -1,14 +1,26 @@
 package com.billcom.notification;
 
 
+import com.billcom.ampq.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages =  {
+        "com.billcom.notification",
+        "com.billcom.ampq",})
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
     }
+
+ /*   @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,NotificationConfig notificationConfig){
+        return args -> {
+            producer.publish("foo",notificationConfig.getInternalExchange());
+        }
+    }*/
 }
